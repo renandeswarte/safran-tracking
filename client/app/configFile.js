3c57ebdb1cad9cc3663ac9d1f4fe14403c0ee529
Renan Deswarte <renandeswarte@gmail.com>
2016-04-21 10:06:59 -0700

    feat: add favicon

---
+++ client/app/configFile.js
@@ -0,0 +1,5 @@
+angular.module('myApp.env', [])
+.constant('FirebaseUrl', "https://safran-tracker.firebaseio.com")
+.constant('DevelopmentAPI', "http://127.0.0.1:3000")
+.constant('ProductionAPI', "https://safran-tracker.herokuapp.com")
+.constant('ServerAPI', "https://safran-tracker.herokuapp.com");
